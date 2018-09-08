7ee875c5e2557fa709cddcbc842e3a14833dd68a
Lydia Batchelor <lydia.l.batchelor@gmail.com>
2018-09-07 19:19:35 -0500

    main, daily req and res, weekly req and res routes

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ app.use('/static', express.static('public'));
 app.set('view engine', handlebars');
 app.set('port', process.argv[2]);
 
-app.get('/', (req,res,next) => {
+app.get('/', function(req,res) {
   var context = {};
   res.render('home', context);
 });
 
-app.get('/daily', (req,res,next) => {
+app.get('/daily', function(req,res) {
   var context = {};
   res.render('daily', context):
 });
 
-app.get('/daily-req', (req,res,next) => {
+app.get('/daily-req', function(req,res) {
     
     var context = {};
     var path = 'https://api.edamam.com/search';
@@ -38,16 +38,17 @@ app.get('/daily-req', (req,res,next) => {
        }
      })
      .then(function (response) {
-       context.label = response.data.rows[0].label;
-       context.image = response.data.rows[0].image;
-       context.source = response.data.rows[0].source;
-       context.url = response.data.rows[0].url;
-       context.yield = response.data.rows[0].yield;
-       context.calories = response.data.rows[0].calories;
-       context.ingredients = response.data.rows[0].ingredients;
-       context.totalNutrients = response.data.rows[0].totalNutrients;
-       context.dietLabels = response.data.rows[0].dietLabels;
-       context.healthLabels = response.data.rows[0].healthLabels;
+       context.uri = response.data.hits[0].recipe.uri;
+       context.label = response.data.hits[0].recipe.label;
+       context.image = response.data.hits[0].recipe.image;
+       context.source = response.data.hits[0].recipe.source;
+       context.url = response.data.hits[0].recipe.url;
+       context.yield = response.data.hits[0].recipe.yield;
+       context.calories = response.data.hits[0].recipe.calories;
+       context.ingredients = response.data.h[0].recipe.ingredients;
+       context.totalNutrients = response.data.rows[0].recipe.totalNutrients;
+       context.dietLabels = response.data.rows[0].recipe.dietLabels;
+       context.healthLabels = response.data.rows[0].recipe.healthLabels;
      })
      .catch(function (error) {
        context.error = error;
@@ -55,6 +56,35 @@ app.get('/daily-req', (req,res,next) => {
      res.render('daily-result', context);
 });
 
+app.get('/weekly-req', function(req,res) {
+
+  var context = {};
+  var path = 'https://api.edamam.com/search';
+
+  axios.get(path, {
+    params: {
+      app_id: id,
+      app_key: key,
+      diet: req.query.diet,
+      excluded: req.query.excluded,
+      health: req.query.health,
+      calories: req.query.min + '-' + req.query.max
+    }
+  })
+  .then(function (response) {
+    var servings = 0;
+    var idx = 0;
+    while(servings < 7 && idx < response.data.count)
+    {
+      context.recipes.push(response.data.hits[idx].recipe);
+      servings += response.data.rows[idx].yield;
+    }
+  })
+  .catch(function (error) {
+    context.error = error;
+  });
+  res.render('weekly-result', context);
+});
 
 app.use(function(req,res){
   res.status(404);
